ca2db2f7174b8283959e63ca9616f89b58aa8b11
Shy Ruparel <Shyamalruparel1991@gmail.com>
2019-03-11 17:53:02 -0400

    Initial Commit

---
+++ handler.py
@@ -0,0 +1,32 @@
+import contentful
+
+client = contentful.Client(
+    "jokk403r56yp",  # This is the space ID. A space is like a project folder in Contentful terms
+    "8b9ddca4bfaa8e3f68520e1211700c6b0fbee29459e7d5b5c776626a3230e8d0",  # This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+)
+
+
+def main(event, context):
+    entry = client.entry("3hPCvDC6URrqtst7zNMkqI")
+    slides = entry.slides
+    markdown = ""
+    for slide in slides:
+        markdown += "".join(["#", slide.title, "\n"])
+        markdown += "".join(["##", slide.subtitle, "\n"])
+        markdown += "".join([slide.body, "\n\n\n"])
+
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/markdown",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": markdown,
+    }
+
+    return response
+
+
+if __name__ == "__main__":
+
+    main("", "")
